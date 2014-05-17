666e9ce7dfe7df459a1711f9dbda9f676d12df03
Adam Bławat <blawat.adam@gmail.com>
2014-05-17 12:02:44 +0200

    Zmodyfikowałem formatowanie kodu.

--- linked-list-int.h
+++ linked-list-int.h
@@ -1,24 +1,23 @@
-#ifndef LINKED_LIST_INT
+#ifndef LINKED_LIST_INT_H
+#define LINKED_LIST_INT_H
 
-    typedef struct linkedListInt
-    {
-        Value                   value;
-            
-        struct linkedListInt    *next;
-        struct linkedListInt    *prev;
-    }
-    LinkedListInt;
+typedef struct linkedListInt
+{
+    Value                   value;
     
-    LinkedListInt ** LinkedListIntCreate();
-    int  LinkedListIntAddFirst(LinkedListInt **list, Value *value);
-    int  LinkedListIntAddLast(LinkedListInt **list, Value *value);
-    void LinkedListIntRemoveFirst(LinkedListInt **list);
-    void LinkedListIntRemoveLast(LinkedListInt **list);
-    void LinkedListIntRemove(LinkedListInt **list, LinkedListInt *toRemove);
-    void LinkedListIntClear(LinkedListInt **list);
-    
-    void LinkedListIntPrint(LinkedListInt **list);
-    
-    #define LINKED_LIST_INT
+    struct linkedListInt    *next;
+    struct linkedListInt    *prev;
+}
+LinkedListInt;
+
+LinkedListInt ** LinkedListIntCreate();
+int  LinkedListIntAddFirst(LinkedListInt **list, Value *value);
+int  LinkedListIntAddLast(LinkedListInt **list, Value *value);
+void LinkedListIntRemoveFirst(LinkedListInt **list);
+void LinkedListIntRemoveLast(LinkedListInt **list);
+void LinkedListIntRemove(LinkedListInt **list, LinkedListInt *toRemove);
+void LinkedListIntClear(LinkedListInt **list);
+
+void LinkedListIntPrint(LinkedListInt **list);
 
 #endif
