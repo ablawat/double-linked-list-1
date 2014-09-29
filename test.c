6482a8dc5c5ea327a46ca254fd01d7c2b4b78abd
Adam Bławat <blawat.adam@gmail.com>
2014-09-29 14:36:16 +0200

    Podział na wiele oddzielnych funkcji testujących.

--- test.c
+++ test.c
@@ -11,7 +11,7 @@ void list_create_test()
     assert(list[1] == NULL);
 }
 
-void list_add_test()
+void list_add_first_test()
 {
     linked_list_int_t **list;
     
@@ -23,8 +23,10 @@ void list_add_test()
     
     linked_list_int_add_first(list, value1);
     
-    assert(list[0] == list[1]);
     assert(list[0] != NULL);
+    assert(list[1] != NULL);
+    assert(list[0] == list[1]);
+    
     assert(list[0] -> value == value1);
     assert(list[0] -> next  == NULL);
     assert(list[0] -> prev  == NULL);
@@ -41,14 +43,24 @@ void list_add_test()
     assert(list[1] -> value == value1);
     assert(list[1] -> next  == NULL);
     assert(list[1] -> prev  == list[0]);
+}
+
+void list_add_last_test()
+{
+    linked_list_int_t **list;
+    
+    int value1 = 1;
+    int value2 = 2;
     
     
     list = linked_list_int_create();
     
     linked_list_int_add_last(list, value1);
     
-    assert(list[0] == list[1]);
     assert(list[0] != NULL);
+    assert(list[1] != NULL);
+    assert(list[0] == list[1]);
+    
     assert(list[0] -> value == value1);
     assert(list[0] -> next  == NULL);
     assert(list[0] -> prev  == NULL);
@@ -67,17 +79,14 @@ void list_add_test()
     assert(list[1] -> prev  == list[0]);
 }
 
-void list_remove_test()
+void list_remove_first_test()
 {
     linked_list_int_t **list;
     
     int value1 = 1;
     int value2 = 2;
-    int value3 = 3;
-    int value4 = 4;
     
     
-    // Test funkcji remove_first()
     list = linked_list_int_create();
     
     linked_list_int_add_last(list, value1);
@@ -85,8 +94,10 @@ void list_remove_test()
     
     linked_list_int_remove_first(list);
     
-    assert(list[0] == list[1]);
     assert(list[0] != NULL);
+    assert(list[1] != NULL);
+    assert(list[0] == list[1]);
+    
     assert(list[0] -> value == value2);
     assert(list[0] -> next  == NULL);
     assert(list[0] -> prev  == NULL);
@@ -95,16 +106,27 @@ void list_remove_test()
     
     assert(list[0] == NULL);
     assert(list[1] == NULL);
+}
+
+void list_remove_last_test()
+{
+    linked_list_int_t **list;
+    
+    int value1 = 1;
+    int value2 = 2;
     
     
-    // Test funkcji remove_last()
+    list = linked_list_int_create();
+    
     linked_list_int_add_last(list, value1);
     linked_list_int_add_last(list, value2);
     
     linked_list_int_remove_last(list);
     
-    assert(list[0] == list[1]);
     assert(list[0] != NULL);
+    assert(list[1] != NULL);
+    assert(list[0] == list[1]);
+    
     assert(list[0] -> value == value1);
     assert(list[0] -> next  == NULL);
     assert(list[0] -> prev  == NULL);
@@ -113,14 +135,25 @@ void list_remove_test()
     
     assert(list[0] == NULL);
     assert(list[1] == NULL);
+}
+
+void list_remove_test()
+{
+    linked_list_int_t **list;
     
-    
-    // Test funkcji remove()
     linked_list_int_t *list_item1;
     linked_list_int_t *list_item2;
     linked_list_int_t *list_item3;
     
-    linked_list_int_t *item_to_remove;
+    linked_list_int_t *to_remove;
+    
+    int value1 = 1;
+    int value2 = 2;
+    int value3 = 3;
+    int value4 = 4;
+    
+    
+    list = linked_list_int_create();
     
     linked_list_int_add_last(list, value1);
     linked_list_int_add_last(list, value2);
@@ -128,8 +161,8 @@ void list_remove_test()
     linked_list_int_add_last(list, value4);
     
     
-    item_to_remove = list[0] -> next -> next;
-    linked_list_int_remove(list, item_to_remove);
+    to_remove = list[0] -> next -> next;
+    linked_list_int_remove(list, to_remove);
     
     assert(list[0] != NULL);
     assert(list[1] != NULL);
@@ -160,8 +193,8 @@ void list_remove_test()
     assert(list_item2 -> prev  == list_item1);
     
     
-    item_to_remove = list[0];
-    linked_list_int_remove(list, item_to_remove);
+    to_remove = list[0];
+    linked_list_int_remove(list, to_remove);
     
     assert(list[0] != NULL);
     assert(list[1] != NULL);
@@ -184,8 +217,8 @@ void list_remove_test()
     assert(list_item2 -> prev  == list_item1);
     
     
-    item_to_remove = list[1];
-    linked_list_int_remove(list, item_to_remove);
+    to_remove = list[1];
+    linked_list_int_remove(list, to_remove);
     
     assert(list[0] != NULL);
     assert(list[1] != NULL);
@@ -200,8 +233,8 @@ void list_remove_test()
     assert(list_item2 -> prev  == list_item1);
     
     
-    item_to_remove = list[0];
-    linked_list_int_remove(list, item_to_remove);
+    to_remove = list[0];
+    linked_list_int_remove(list, to_remove);
     
     assert(list[0] == NULL);
     assert(list[1] == NULL);
@@ -211,7 +244,12 @@ void list_remove_test()
 int main()
 {
     list_create_test();
-    list_add_test();
+    
+    list_add_first_test();
+    list_add_last_test();
+    
+    list_remove_first_test();
+    list_remove_last_test();
     list_remove_test();
     
     return 0;
